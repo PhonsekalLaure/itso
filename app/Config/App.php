45abc05de0f9c6103bded930c36ae76c03c5e3d4
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2025-11-17 16:37:20 +0800

    fix: update baseURL to match project directory

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/final/';
+    public string $baseURL = 'http://localhost/itso/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
